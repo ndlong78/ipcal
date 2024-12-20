24ce9f152bbda3ddebf7c49c503f1fb4f531485e
ndlong78 <33077621+ndlong78@users.noreply.github.com>
2024-12-20 13:08:32 +0700

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@
 from flask import Flask
 
 def create_app():
-    app = Flask(__name__, template_folder='/opt/ipcal/templates')
+    app = Flask(__name__)
 
     from .routes import main_bp
     app.register_blueprint(main_bp)
