0f604aa7eca376d8a09a90396f0c24014a638e9d
ndlong78 <33077621+ndlong78@users.noreply.github.com>
2024-12-24 10:35:31 +0700

    update

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,7 @@ import logging
 import warnings
 from flask import Blueprint, render_template, request, flash
 import re
+import ipaddress
 from .calculations import calculate_ipv4, calculate_ipv6, calculate_network_and_subnet
 from .ip_to_regex import ip_to_regex, validate_regex
 
@@ -26,11 +27,10 @@ def index():
 def calculate():
     result = {}
 
-    ip_type = request.form.get('ip-type')
     ip_address = filter_ip_input(request.form.get('ip-address'))
     network_input = filter_ip_input(request.form.get('network'))
 
-    logging.debug(f"IP type: {ip_type}, IP address: {ip_address}, Network input: {network_input}")
+    logging.debug(f"IP address: {ip_address}, Network input: {network_input}")
 
     if not network_input:
         error_message = "Network input is required."
@@ -38,34 +38,29 @@ def calculate():
         flash(error_message, 'error')
         return render_template('index.html')
 
-    if ip_type == 'ipv4':
-        if ip_address:
-            ipv4_result = calculate_ipv4(ip_address)
-            if 'error' in ipv4_result:
-                warnings.warn(f"Invalid IPv4 address: {ip_address}")
-            result['ip'] = ipv4_result
-        network_result = calculate_network_and_subnet(ip_address, network_input, ip_version='ipv4')
-        if 'error' in network_result:
-            warnings.warn(f"Invalid network input for IPv4: {network_input}")
-        result['network'] = network_result
-        if 'error' not in network_result:
-            cidr = f"{network_result['Network Address']}/{network_result['CIDR']}"
-            regex_pattern = ip_to_regex(cidr)
-            result['regex'] = {"pattern": regex_pattern}
+    try:
+        ip = ipaddress.ip_address(ip_address)
+        if ip.version == 4:
+            ip_result = calculate_ipv4(ip_address)
+            network_result = calculate_network_and_subnet(ip_address, network_input, ip_version='ipv4')
+        else:
+            ip_result = calculate_ipv6(ip_address)
+            network_result = calculate_network_and_subnet(ip_address, network_input, ip_version='ipv6')
 
-    elif ip_type == 'ipv6':
-        if ip_address:
-            ipv6_result = calculate_ipv6(ip_address)
-            if 'error' in ipv6_result:
-                warnings.warn(f"Invalid IPv6 address: {ip_address}")
-            result['ip'] = ipv6_result
-        network_result = calculate_network_and_subnet(ip_address, network_input, ip_version='ipv6')
         if 'error' in network_result:
-            warnings.warn(f"Invalid network input for IPv6: {network_input}")
+            warnings.warn(f"Invalid network input: {network_input}")
+        
+        result['ip'] = ip_result
         result['network'] = network_result
+
         if 'error' not in network_result:
             cidr = f"{network_result['Network Address']}/{network_result['CIDR']}"
             regex_pattern = ip_to_regex(cidr)
             result['regex'] = {"pattern": regex_pattern}
+    except ValueError:
+        error_message = "Invalid IP address."
+        warnings.warn(error_message)
+        flash(error_message, 'error')
+        return render_template('index.html')
 
     return render_template('index.html', result=result)
